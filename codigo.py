0969a91958f844e5d944119ef83af089f406bbc8
Noel <noelcerrom@gmail.com>
2021-06-05 14:54:15 +0200

    primera parte

--- codigo.py
+++ codigo.py
@@ -1,1 +1,24 @@
-print("Hola mundo")
+frase = "Hola que tal desde python"
+print(len(frase))
+print(frase[-5])
+print(frase[:])
+print(frase[3:4])
+print(frase[:4])
+
+frase = "Hola me llamo Noel"
+print(frase)
+nombre = "Noel"
+print(f"hola me llamo {nombre}")
+
+
+# Tipado
+texto = "texto"
+numero = 45
+verdadero = True
+decimal = 4.5
+print(type(decimal))
+
+# Entrada por teclado
+edad = int(input("Intrduce tu edad "))
+suma = edad + 20
+print(f"tu edad mas 20 es {suma}")
